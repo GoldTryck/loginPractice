abfe3f8de96402fe782a006f06a631039a901962
Hector Larios <lariosh69@gmail.com>
2025-05-28 18:15:22 -0600

    Configuracion final

--- src/main/java/com/daw/loginPractice/dtos/ErrorResponse.java
+++ src/main/java/com/daw/loginPractice/dtos/ErrorResponse.java
@@ -3,6 +3,7 @@ package com.daw.loginPractice.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +11,10 @@ import lombok.NoArgsConstructor;
 public class ErrorResponse {
     private int status;
     private String message;
+    private Map<String, String> errors;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
--- src/main/java/com/daw/loginPractice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/daw/loginPractice/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.daw.loginPractice.exceptions;
 import com.daw.loginPractice.dtos.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +19,17 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
-
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validación fallida",
+                errors
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
